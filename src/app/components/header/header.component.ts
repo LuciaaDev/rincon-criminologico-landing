a224b6887106f8769b5e22c2ce31024157032653
Lucía Ruiz <luciaruiz9722@gmail.com>
2025-04-02 14:32:46 +0200

    feat: build first structure of header and add fonts and translations

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { TranslocoModule } from "@jsverse/transloco";
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [TranslocoModule],
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
     
+    constructor(private router: Router) {}
+
+    /**
+     * To go to home landing clicking on logo.
+     */
+    goToHomeLanding(): void {
+        this.router.navigate(['']);
+    }
 }
