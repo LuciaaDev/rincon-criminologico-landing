92ac0ed19529e830c114116a74ed922c68b92649
Lucía Ruiz <luciaruiz9722@gmail.com>
2025-05-16 12:53:05 +0200

    feat: resolve unit testing of button component

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { TranslocoService, TranslocoTestingModule } from "@jsverse/transloco";
 import { ButtonComponent } from "./button.component";
+import { CommonModule } from "@angular/common";
 
 describe('ButtonComponent', () => {
     let component: ButtonComponent;
-    let fixture: ComponentFixture<ButtonComponent>;
     let translocoServiceMock = TranslocoService;
 
     beforeEach(async () => {
@@ -12,20 +12,45 @@ describe('ButtonComponent', () => {
             imports: [
                 ButtonComponent, 
                 TranslocoTestingModule,
+                CommonModule
             ],
             providers: [
                 {component: TranslocoService, useClass: translocoServiceMock}
             ]
         }).compileComponents();
-    });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(ButtonComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        component = new ButtonComponent();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should emit the event if button is not disabled', () => {
+        component.disabled = false;
+        const emitSpyOn = jest.spyOn(component.clickBtnChange, 'emit');
+
+        component.onClickButton();
+        expect(emitSpyOn).toHaveBeenCalled();
+    });
+
+    it('should not emit the event if button is disabled', () => {
+        component.disabled = true;
+        const emitSpyOn = jest.spyOn(component.clickBtnChange, 'emit');
+
+        component.onClickButton();
+        expect(emitSpyOn).not.toHaveBeenCalled();
+    });
+
+    it('should accept all @Input()', () => {
+        component.text = 'test';
+        component.color = 'primary';
+        component.size = 'large';
+        component.disabled = false;
+
+        expect(component.text).toBe('test');
+        expect(component.color).toBe('primary');
+        expect(component.size).toBe('large');
+        expect(component.disabled).toBe(false);
     });
 });
