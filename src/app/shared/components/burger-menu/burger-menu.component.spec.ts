8f2a858328dbd0e64b2374d47cf50ec65110aaa5
Lucía Ruiz <luciaruiz9722@gmail.com>
2025-05-16 13:17:10 +0200

    feat: resolve unit testing of burger menu component

--- src/app/shared/components/burger-menu/burger-menu.component.spec.ts
+++ src/app/shared/components/burger-menu/burger-menu.component.spec.ts
@@ -1,10 +1,11 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
 import { TranslocoService, TranslocoTestingModule } from "@jsverse/transloco";
 import { BurgerMenuComponent } from "./burger-menu.component";
+import { RouterModule } from "@angular/router";
+import { BurgerMenuItem } from "../../interfaces/shared-components.model";
 
 describe('BurgerMenuComponent', () => {
     let component: BurgerMenuComponent;
-    let fixture: ComponentFixture<BurgerMenuComponent>;
     let translocoServiceMock = TranslocoService;
 
     beforeEach(async () => {
@@ -12,20 +13,50 @@ describe('BurgerMenuComponent', () => {
             imports: [
                 BurgerMenuComponent, 
                 TranslocoTestingModule,
+                RouterModule
             ],
             providers: [
                 {component: TranslocoService, useClass: translocoServiceMock}
             ]
         }).compileComponents();
-    });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(BurgerMenuComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        component = new BurgerMenuComponent();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should open or close the burger menu and reset activeDropdownOption', () => {
+        component.isMenuOpen = false;
+        component.activeDropdownOption = 'header.language';
+
+        component.toggleMenu();
+
+        expect(component.isMenuOpen).toBeTruthy();
+        expect(component.activeDropdownOption).toBeNull();
+    });
+
+    it('should active dropdown option if it is deactivated', () => {
+        component.activeDropdownOption = null;
+        component.activateDropdownOption('header.language');
+        expect(component.activeDropdownOption).toBe('header.language');
+    });
+
+    it('should deactivate dropdown option if it is activated', () => {
+        component.activeDropdownOption = 'header.language';
+        component.activateDropdownOption('header.language');
+        expect(component.activeDropdownOption).toBeNull();
+    });
+
+    it('should accept all @Input()', () => {
+        const mockItems = [
+        { label: 'header.about_me', type: 'link', action: () => {} }
+        ] as BurgerMenuItem[];
+
+        component.menuItemsList = mockItems;
+
+        expect(component.menuItemsList.length).toBe(1);
+        expect(component.menuItemsList[0].label).toBe('header.about_me');
     });
 });
