ca70019987b4addd7db37c12d4d1147a83804a4b
Lucía Ruiz <luciaruiz9722@gmail.com>
2025-05-19 15:08:31 +0200

    feat: add responsive styles of slogan content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TranslocoModule } from "@jsverse/transloco";
 import { ButtonComponent } from "../../shared/components/button/button.component";
 
@@ -9,6 +9,20 @@ import { ButtonComponent } from "../../shared/components/button/button.component
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    public showSloganImg = false;
+
+    ngOnInit(): void {
+        this.checkScreenSize();
+    }
     
+    /**
+     * checkScreenSize
+     * To show action buttons or burger menu depending on 
+     * the size of the screen.
+     */
+    public checkScreenSize(): void {
+        this.showSloganImg = window.innerWidth >= 768;
+    }
 }
